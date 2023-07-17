e3e03cbcc643b8d67c5860523bfd09720503f5e8
JhonAbner <136210735+JhonAbner@users.noreply.github.com>
2023-07-17 20:08:48 -0300

    Update login.php

--- login.php
+++ login.php
@@ -8,9 +8,9 @@
 <body>
     <form method="post" action='login.php'>
             USUÁRIO: <input TYPE='text' name='usuario' ><br><br>
-            SENHA: <input type='password' name= 'senha'  ><br><br>
+            SENHA: <input type='password' name= 'senha'  ><br>
+            <a href="recuperar.php">Esqueceu senha?</a><br><br>
             <input type='submit' name='enviar' value='Acessar' ><br><br>
-            <a href="recuperar.php">Esqueceu senha?</a><br>
             <p>Ainda não tem conta? <a href="cod.php">Cadastre-se</a><br><br></p>
             </form>  
     <?php
