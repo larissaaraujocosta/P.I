61d005a037b12ab4ebf4e131b5531d1662f50bc6
JhonAbner <136210735+JhonAbner@users.noreply.github.com>
2023-07-17 19:50:54 -0300

    Update cad.php

--- cad.php
+++ cad.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="post">   
-        <b>Nome: </b> <input name="Nome" type="text" ><br><br>
-        <b>Número de matricula:  </b> <input name="matricula" type="number"> <br><br>
-        <b>CPF: </b> <input name="cpf" type="text" ><br><br>
-        <b>Telefone: </b> <input name="Telefone" type="text"><br><br>
-        <b>E-MAIL: </b> <input name="Email" type="text" ><br><br>
-        <input type="submit" value="enviar" name="enviar"> <input type="reset" value="Limpar"><br><br><br>
-                
-    </form>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post">   
+            <b>Nome: </b> <input name="Nome" type="text" ><br><br>
+            <b>Número de matricula:  </b> <input name="matricula" type="number"> <br><br>
+            <b>CPF: </b> <input name="cpf" type="text" ><br><br>
+            <b>Telefone: </b> <input name="Telefone" type="text"><br><br>
+            <b>E-MAIL: </b> <input name="Email" type="text" ><br><br>
+            <input type="submit" value="enviar" name="enviar"> <input type="reset" value="Limpar"><br><br><br>
+                    
+        </form>
         
 </body>
 </html>
