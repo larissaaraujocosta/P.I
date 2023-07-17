0841b30e992159da825711e6ab334e291d0dbe6d
JhonAbner <136210735+JhonAbner@users.noreply.github.com>
2023-07-17 20:00:29 -0300

    Update login.php

--- login.php
+++ login.php
@@ -11,13 +11,13 @@
             SENHA: <input type='password' name= 'senha'  ><br><br>
             <input type='submit' name='enviar' value='Acessar' ><br><br>
             <input type='submit' name='mdsenha' value='Esqueceu senha?' ><br>
-            <p>Ainda não tem conta? <input type='submit' name='cadastro' value='Cadastrar usuário' ><br><br></p>
+            <p>Ainda não tem conta? <a href="cod.php">Cadastre-se</a><br><br></p>
             </form>  
     <?php
         extract($_POST);
         
-        if (isset($cadastro)){
-            header('Location: cad.php' );
+        if (isset($enviar)){
+            
         }
     ?>
 </body>
